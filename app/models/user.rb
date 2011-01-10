2353a4aa2083351359ceebb2b714c38856b34524
sabarish48 <sabarishsankar89@yahoo.co.in>
2011-01-10 23:59:19 +0530

    checking cron

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,11 @@ class User < ActiveRecord::Base
 
   def self.send_birthday_notification
     birthdays = Array.new
+    puts "hello checking"
     User.find(:all).each do |user|
       dob = user.date_of_birth
       if dob.day == Time.now.day && dob.month == Time.now.month
+        puts "hello working"
         birthdays << user
       end
     end
